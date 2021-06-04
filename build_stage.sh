c5220a7f3a6936724071f7f14c8b9c74e767de68
Athan Clark <athan.clark@gmail.com>
2021-06-04 13:11:12 -0400

    starting to make legitimate functions

--- build_stage.sh
+++ build_stage.sh
@@ -9,7 +9,9 @@ export WD=`pwd`
 sudo -i -u postgres psql -d localcooking_tmp -f $WD/stage.template.sql
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ users" > tmp/users
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ pending_registrations" > tmp/pendingRegistrations
+sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ active_pending_registrations" > tmp/activePendingRegistrations
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ sessions" > tmp/sessions
+sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ active_sessions" > tmp/activeSessions
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ chefs" > tmp/chefs
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ credentials" > tmp/credentials
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ chef_credentials" > tmp/chefCredentials
@@ -22,10 +24,12 @@ sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ orders" > tmp/orders
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ order_contents" > tmp/orderContents
 sudo -i -u postgres psql -d localcooking_tmp -c "\\d+ reviews" > tmp/reviews
 
-ltext "stage.template.sql tmp/users tmp/pendingRegistrations tmp/sessions tmp/chefs tmp/credentials tmp/chefCredentials tmp/menus tmp/items tmp/itemRevisions tmp/menuItemMapping tmp/carts tmp/orders tmp/orderContents tmp/reviews" \
+ltext "stage.template.sql tmp/users tmp/pendingRegistrations tmp/activePendingRegistrations tmp/sessions tmp/activeSessions tmp/chefs tmp/credentials tmp/chefCredentials tmp/menus tmp/items tmp/itemRevisions tmp/menuItemMapping tmp/carts tmp/orders tmp/orderContents tmp/reviews" \
       -r "tmp/users" \
       -r "tmp/pendingRegistrations" \
+      -r "tmp/activePendingRegistrations" \
       -r "tmp/sessions" \
+      -r "tmp/activeSessions" \
       -r "tmp/chefs" \
       -r "tmp/credentials" \
       -r "tmp/chefCredentials" \
